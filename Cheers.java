5ccd90eacbe10212d95744ee88b1080c1040be22
RotemKulka <rotem.kulka@post.runi.ac.il>
2025-11-13 02:34:11 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,21 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = args[0];
+            int num = Integer.parseInt(args[1]);
+            String wordUpper = word.toUpperCase();
+            String an = "AEFHILMNORSX";
+            for (int i=0; i<word.length(); i++){
+                char ch = wordUpper.charAt(i);
+                if (an.indexOf(ch) == -1){
+                        System.out.println("Give me a  " + ch + ": " + ch + "!");
+                } else {
+                        System.out.println("Give me an " + ch + ": " + ch + "!");
+                }
+            }
+            System.out.println("What does that spell?");
+            for (int j=0; j<num; j++){
+                System.out.println(wordUpper + "!!!");
+            }
         }
 }
+
